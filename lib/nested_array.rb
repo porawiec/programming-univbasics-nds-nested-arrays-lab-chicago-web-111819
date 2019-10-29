b9541d365d5f69274204bd93a80d8f0f39736120
Paul O <paul.orawiec@gmail.com>
2019-10-29 08:49:12 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -27,13 +27,13 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   infinite_jest = [
-    :house => [
+    [
       "Don Gately",
       "Joelle van Dyne",
       "Pat M.",
       "Kate Gompert",
       "Bruce Green"],
-    :tennis_academy => [
+    [
       "Hal Incandenza",
       "Lyle",
       "Gerhard Schtitt",
